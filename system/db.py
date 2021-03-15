f81b1b1609f9cbc25c6957429cb664dd9e335646
a5892731 <a5892731@o2.pl>
2021-03-15 20:32:34 +0100

    in progress

--- system/db.py
+++ system/db.py
@@ -14,42 +14,44 @@ class Database:
 
     version = "1.0"
     version_date = "2021-03-15"
-    version_info = "In preparation"
-    # system need password protection
+    version_info = ""
 
     def __init__(self, db_name, host_name, user_name, user_password, tables):
         self.db_name = db_name
         self.host_name = host_name
         self.user_name = user_name
-        self.user_password = user_password
+        #self.user_password = user_password # password is protected
 
-        connection = self.create_connection_to_server()
+        connection = self.create_connection_to_server(user_password)
         create_database_query = "CREATE DATABASE {}".format(db_name)
-        self.create_database(connection, create_database_query, tables)
+        self.create_database(connection, create_database_query, tables, user_password)
 
-    def create_connection_to_server(self):
+
+    def create_connection_to_server(self, user_password):
         connection = None
         try:
             connection = mysql.connector.connect(
                 host=self.host_name,
                 user=self.user_name,
-                passwd=self.user_password
+                passwd=user_password
             )
             print(">>> Connection to MySQL server successful")
         except Error as e:
             print(f">>> The error '{e}' occurred")
         return connection
 
-    def create_database(self, connection, query, tables):
+    def create_database(self, connection, query, tables, user_password):
         cursor = connection.cursor()
         try:
             cursor.execute(query)
             print(">>> Database created successfully")
+            self.connection = self.create_connection_to_db(user_password)
             self.create_tables(tables) # if database just been created then you need to build db tables too
 
         except Error as e:
             if "1007 (HY000)" in f"{e}":
                 print(">>> Database exists")
+                self.connection = self.create_connection_to_db(user_password)
             else:
                 print(f">>> The error '{e}' occurred")
 
@@ -63,8 +65,6 @@ class Database:
                                                  # "test_table1_id INT AUTO_INCREMENT" and you nead test_table1_id
             return columns
 
-        connection = self.create_connection_to_db()
-
         for key in tables:
             create_table = """
             CREATE TABLE IF NOT EXISTS {} (
@@ -72,15 +72,15 @@ class Database:
             ) ENGINE = InnoDB 
             """.format(key, create_columns(key, tables))
 
-            self.execute_query(connection, create_table, "DB {} table created successfully".format(key))
+            self.execute_query(self.connection, create_table, "DB {} table created successfully".format(key))
 
-    def create_connection_to_db(self):
+    def create_connection_to_db(self, user_password):
         connection = None
         try:
             connection = mysql.connector.connect(
                 host= self.host_name,
                 user= self.user_name,
-                passwd= self.user_password,
+                passwd= user_password,
                 database= self.db_name
             )
             print(">>> Connection to MySQL DB successful")
@@ -119,9 +119,8 @@ class Database:
         except Error as e:
             print(f">>> The error '{e}' occurred")
 
-    def del_db(self):
-        connection = self.create_connection_to_db()
-        self.execute_query(connection, "DROP DATABASE {}".format(self.db_name))
+    def del_db(self, message = "Database deleted"):
+        self.execute_query(self.connection, "DROP DATABASE {}".format(self.db_name), message)
 
     def __del__(self):
         return ">>> Class deleted"
@@ -147,10 +146,11 @@ if __name__ == "__main__":
 
     tables2 = DataImport("TABLES.txt", "dict", "db_configuration")
 
-    #print(tables2())
-
     test = Database("spacex", "127.0.0.1", "root", "", tables2())
-    #print(test.__del__())
+
+    test.del_db()
+    print(test.__del__())
 
     os.chdir("system")
 
+#-----------------------------------------------------------------------------------------------------------------------
