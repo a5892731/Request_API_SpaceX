23a7a03ff62778942871c48c117605f5ae3439cb
a5892731 <a5892731@o2.pl>
2021-03-17 15:31:05 +0100

    in progress

--- system/system_states.py
+++ system/system_states.py
@@ -8,28 +8,32 @@ https://python-statemachine.readthedocs.io/en/latest/readme.html#getting-started
 '''
 from statemachine import StateMachine, State
 
+from statemachine import StateMachine, State
+
 class TrafficLightMachine(StateMachine):
+    "A traffic light machine"
     green = State('Green', initial=True)
     yellow = State('Yellow')
     red = State('Red')
 
-    slowdown = green.to(yellow)
-    stop = yellow.to(red)
-    go = red.to(green)
+    cycle = green.to(yellow) | yellow.to(red) | red.to(green)
 
+    def on_enter_green(self):
+        print('Valendo!')
 
+    def on_enter_yellow(self):
+        print('Calma, lá!')
+
+    def on_enter_red(self):
+        print('Parou.')
 
 
 
 
 if __name__ == "__main__":
 
-
-    traffic_light = TrafficLightMachine()
-
-    print(traffic_light.current_state)
-
-
-
-
-    print(traffic_light.current_state == TrafficLightMachine.green == traffic_light.green)
+    stm = TrafficLightMachine()
+    stm.cycle()
+    stm.cycle()
+    stm.cycle()
+    stm.cycle()
