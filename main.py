35f076d16e2109bc87fbbebebbfe8553c5a59263
a5892731 <a5892731@o2.pl>
2021-03-17 15:19:29 +0100

    in progress

--- main.py
+++ main.py
@@ -1,115 +1,37 @@
 '''
 version 3.0 - in preparation!!! get version 2.0 if you want to get some spacex data ;)
 author: a5892731
-date: 2021-03-02
+date: 2021-03-17
 
 required modules:
 
 pip install terminaltables
 pip install requests
 pip install json
+pip install python-statemachine
 
 os , platform, datetime
 '''
 
 from system.user_selection import UserStateSelection
-from system.system_states import SystemStates
 from system.menu import SmartMenu, clear_screen
 from system.read_data_files import DataImport
 
+from system.system_states import TrafficLightMachine
+
+
+
 #--------------------------------------------------------------------------------------- <<<< MAIN MENU
+
+
 
 if __name__ == "__main__":
 
-    HEADING_MENU_DICT = {1: "BOOSTERS", 2: "CAPSULES", 3: "LAUNCHES"}
-    BOOSTERS_MENU_DICT = {11 : "STATUS", 12: "SERIAL", 13: "GET LAUNCHES DATA"}
-    BOOSTERS_STATUS_DICT = {111: "active", 112: "inactive", 113: "unknown",
-                            114: "inactive", 115: "expended", 116: "lost"}
-    CAPSULES_MENU_DICT = {21 : "STATUS", 22: "SERIAL", 23: "GET LAUNCHES DATA"}
-    CAPSULES_STATUS_DICT = {211: "active", 212: "inactive", 213: "unknown",
-                            214: "inactive", 215: "expended", 216: "lost"}
-    MISSIONS_DICT = {31 : "PREVIOUS", 32 : "FUTURE", 33: "OBJECT NUMBER", 34: "FLIGHT NUMBER",
-                     35: "SORT OBJECTS\n    BY TIME", 36: "GET CREW DATA"}
-
-    MENU_DICT = [
-                HEADING_MENU_DICT,
-                [BOOSTERS_MENU_DICT, CAPSULES_MENU_DICT, MISSIONS_DICT],
-                [BOOSTERS_STATUS_DICT, CAPSULES_STATUS_DICT, {}]
-                ]
-
-    BOOSTERS_OBJECT_CALL_LIST = DataImport("BOOSTERS_OBJECT_LIST.txt", "list")
-    SHORT_BOOSTERS_OBJECT_CALL_LIST = DataImport("SHORT_BOOSTERS_OBJECT_LIST.txt", "list")
-    VERY_SHORT_BOOSTERS_OBJECT_CALL_LIST = DataImport("VERY_SHORT_BOOSTERS_OBJECT_LIST.txt", "list")
-
-    CAPSULES_OBJECT_CALL_LIST = DataImport("CAPSULES_OBJECT_LIST.txt", "list")
-    SHORT_CAPSULES_OBJECT_CALL_LIST = DataImport("SHORT_CAPSULES_OBJECT_LIST.txt", "list")
-    VERY_SHORT_CAPSULES_OBJECT_CALL_LIST = DataImport("VERY_SHORT_CAPSULES_OBJECT_LIST.txt", "list")
-
-    LAUNCHES_OBJECT_CALL_LIST = DataImport("LAUNCHES_OBJECT_LIST.txt", "list")
-    SHORT_LAUNCHES_OBJECT_CALL_LIST = DataImport("SHORT_LAUNCHES_OBJECT_LIST.txt", "list")
-    VERY_SHORT_LAUNCHES_OBJECT_CALL_LIST = DataImport("VERY_SHORT_LAUNCHES_OBJECT_LIST.txt", "list")
-
-    CREW_OBJECT_CALL_LIST = DataImport("CREW_OBJECT_LIST.txt", "list")
-    VERY_SHORT_CREW_OBJECT_CALL_LIST = DataImport("CREW_OBJECT_LIST.txt", "list")
-
-    API_ADDRESS_CALL_DICT = DataImport("API_ADDRESS_DICT.txt", "dict")
-
-    state_machine = SystemStates()
-    menu = SmartMenu(MENU_DICT)
-
-    state = 0
-
-    # --------------------------------------------------------------------------------------- <<<< MAIN LOOP
-
     while True:
-        menu.cover_menu_by_state(state)
-        menu.drow_menu()
 
-        if state >= 111 and state < 118:
-            state_machine.booster_status_state(BOOSTERS_OBJECT_CALL_LIST(), VERY_SHORT_BOOSTERS_OBJECT_CALL_LIST(),
-                                               API_ADDRESS_CALL_DICT()["BOOSTERS"], BOOSTERS_STATUS_DICT[state],
-                                               "status")
-        elif state == 12:
-            state_machine.booster_serial_state(BOOSTERS_OBJECT_CALL_LIST(), SHORT_BOOSTERS_OBJECT_CALL_LIST(),
-                                               API_ADDRESS_CALL_DICT()["BOOSTERS"],
-                                               input(">>> Put booster serial number: "), "serial")
-        elif state == 13:
-            state_machine.get_launches_data_for_boosters_state(BOOSTERS_OBJECT_CALL_LIST(), API_ADDRESS_CALL_DICT()["BOOSTERS"],
-                                                               LAUNCHES_OBJECT_CALL_LIST(), API_ADDRESS_CALL_DICT()["LAUNCHES"])
-        elif state >= 211 and state < 217:
-            state_machine.capsule_status_state(CAPSULES_OBJECT_CALL_LIST(), VERY_SHORT_CAPSULES_OBJECT_CALL_LIST(),
-                                               API_ADDRESS_CALL_DICT()["CAPSULES"], CAPSULES_STATUS_DICT[state],
-                                               "status")
-        elif state == 22:
-            state_machine.capsule_serial_state(CAPSULES_OBJECT_CALL_LIST(), SHORT_CAPSULES_OBJECT_CALL_LIST(),
-                                               API_ADDRESS_CALL_DICT()["CAPSULES"],
-                                               input(">>> Put capsule serial number: "), "serial")
-        elif state == 23:
-            state_machine.get_launches_data_for_capsules_state(CAPSULES_OBJECT_CALL_LIST(), API_ADDRESS_CALL_DICT()["CAPSULES"],
-                                                               LAUNCHES_OBJECT_CALL_LIST(), API_ADDRESS_CALL_DICT()["LAUNCHES"])
-        elif state == 31:
-            state_machine.launches_previouse(LAUNCHES_OBJECT_CALL_LIST(), VERY_SHORT_LAUNCHES_OBJECT_CALL_LIST(),
-                                             API_ADDRESS_CALL_DICT()["LAUNCHES"])
-        elif state == 32:
-            state_machine.launches_future(LAUNCHES_OBJECT_CALL_LIST(), SHORT_LAUNCHES_OBJECT_CALL_LIST(),
-                                          API_ADDRESS_CALL_DICT()["LAUNCHES"])
-        elif state == 33:
-            state_machine.launches_object_number_state(LAUNCHES_OBJECT_CALL_LIST(), LAUNCHES_OBJECT_CALL_LIST(),
-                                                       API_ADDRESS_CALL_DICT()["LAUNCHES"],
-                                                       input(">>> Put launch object number: "), "OBJECT NUMBER")
-        elif state == 34:
-            state_machine.launches_flight_number_state(LAUNCHES_OBJECT_CALL_LIST(), LAUNCHES_OBJECT_CALL_LIST(),
-                                                       API_ADDRESS_CALL_DICT()["LAUNCHES"],
-                                                       input(">>> Put flight number: "), "flight_number")
-        elif state == 35:
-            state_machine.launches_sort_by_time(LAUNCHES_OBJECT_CALL_LIST(), [], API_ADDRESS_CALL_DICT()["LAUNCHES"])
+        system_stete = TrafficLightMachine()
 
-        elif state == 36:
-            state_machine.get_crew_data_state(LAUNCHES_OBJECT_CALL_LIST(), API_ADDRESS_CALL_DICT()["LAUNCHES"],
-                                    CREW_OBJECT_CALL_LIST(), API_ADDRESS_CALL_DICT()["CREW"])
+        system_stete.current_state
 
 
 
-        user = UserStateSelection(state)
-        state = user.return_state()
-        clear_screen()
--- system/system_states.py
+++ system/system_states.py
@@ -1,125 +1,18 @@
 '''
-file version 2.3
+version 3.0 - in preparation!!! get version 2.0 if you want to get some spacex data ;)
+author: a5892731
+date: 2021-03-17
 
-this is a state machine file
+https://python-statemachine.readthedocs.io/en/latest/readme.html#getting-started
+
 '''
+from statemachine import StateMachine, State
 
+class TrafficLightMachine(StateMachine):
+    green = State('Green', initial=True)
+    yellow = State('Yellow')
+    red = State('Red')
 
-from system.spacex_data_container import SpacexObjects, ObjectsSort
-
-class SystemStates:
-
-    def __init__(self):
-
-        self.boosters = []
-        self.capsules = []
-        self.launches = []
-        self.crew = []
-    # ------------------------------------------------------------------------------------------------------------------
-    def booster_status_state(self, BOOSTERS_LIST, ITEMS_TO_DISPLAY_LIST, API_ADDRESS, status_value, status):
-        self.boosters = self.execute_standard_key_value_state(BOOSTERS_LIST, ITEMS_TO_DISPLAY_LIST, API_ADDRESS,
-                                                              self.boosters, status_value, status)
-
-    def booster_serial_state(self, BOOSTERS_LIST, ITEMS_TO_DISPLAY_LIST, API_ADDRESS, serial_value, serial):
-        self.boosters = self.execute_standard_key_value_state(BOOSTERS_LIST, ITEMS_TO_DISPLAY_LIST, API_ADDRESS,
-                                                              self.boosters, serial_value, serial)
-
-    def capsule_status_state(self, CAPSULES_LIST, ITEMS_TO_DISPLAY_LIST, CAPSULES_API_ADDRESS, status_value, status):
-
-        self.capsules = self.execute_standard_key_value_state(CAPSULES_LIST, ITEMS_TO_DISPLAY_LIST, CAPSULES_API_ADDRESS,
-                                                              self.capsules, status_value, status)
-
-    def capsule_serial_state(self, CAPSULES_LIST, ITEMS_TO_DISPLAY_LIST, API_ADDRESS, serial_value, serial):
-        self.capsules = self.execute_standard_key_value_state(CAPSULES_LIST, ITEMS_TO_DISPLAY_LIST, API_ADDRESS,
-                                                              self.capsules, serial_value, serial)
-
-    def launches_object_number_state(self, LAUNCHES_LIST, ITEMS_TO_DISPLAY_LIST, API_ADDRESS,
-                                     object_number_value, object_number):
-
-        self.launches = self.execute_standard_key_value_state(LAUNCHES_LIST, ITEMS_TO_DISPLAY_LIST, API_ADDRESS,
-                                                              self.launches,
-                                                              object_number_value, object_number)
-
-    def launches_flight_number_state(self, LAUNCHES_LIST, ITEMS_TO_DISPLAY_LIST, API_ADDRESS,
-                                    flight_number_value, flight_number):
-
-        self.launches = self.execute_standard_key_value_state(LAUNCHES_LIST, ITEMS_TO_DISPLAY_LIST, API_ADDRESS,
-                                                             self.launches,
-                                                             flight_number_value, flight_number)
-    # ------------------------------------------------------------------------------------------------------------------
-    def launches_previouse(self, LAUNCHES_LIST, ITEMS_TO_DISPLAY_LIST, API_ADDRESS, value = "previosue", key = "time"):
-        so = SpacexObjects(API_ADDRESS, LAUNCHES_LIST, self.launches)
-        objects = so.objects
-        sorted_missions = ObjectsSort(objects)
-        sorted_missions.print_objects_by_previouse_time(ITEMS_TO_DISPLAY_LIST)
-        self.launches = objects
-
-    def launches_future(self, LAUNCHES_LIST, ITEMS_TO_DISPLAY_LIST, API_ADDRESS, value = "future", key = "time"):
-        so = SpacexObjects(API_ADDRESS, LAUNCHES_LIST, self.launches)
-        objects = so.objects
-        sorted_missions = ObjectsSort(objects)
-        sorted_missions.print_objects_by_future_time(ITEMS_TO_DISPLAY_LIST)
-        self.launches = objects
-    # ------------------------------------------------------------------------------------------------------------------
-    def launches_sort_by_time(self, LAUNCHES_LIST, ITEMS_TO_DISPLAY_LIST, API_ADDRESS, value = "UP", key = "time"):
-        so = SpacexObjects(API_ADDRESS, LAUNCHES_LIST, self.launches)
-        objects = so.objects
-        sorted_missions = ObjectsSort(objects)
-        self.launches = sorted_missions.sort_objects_by_time()
-        print(">>> data sorted by {}!".format(key))
-    # ------------------------------------------------------------------------------------------------------------------
-    def get_crew_data_state(self, LAUNCHES_LIST, API_ADDRESS,
-                            CREW_LIST, CREW_API_ADDRESS,
-                            first_object_key = "crew", second_object_key = "id", replace_key = "name"):
-
-        self.launches = self.replace_id_whith_elements_in_object_list_standard_state(LAUNCHES_LIST, API_ADDRESS, self.launches,
-                                                                                    CREW_LIST, CREW_API_ADDRESS, self.crew,
-                                                                                    first_object_key,
-                                                                                    second_object_key,
-                                                                                    replace_key)
-    def get_launches_data_for_capsules_state(self, CAPSULES_LIST, CAPSULES_API_ADDRESS,
-                                             LAUNCHES_LIST, LAUNCHES_API_ADDRESS,
-                                             first_object_key = "launches", second_object_key = "id", replace_key = "flight_number"):
-
-        self.capsules = self.replace_id_whith_elements_in_object_list_standard_state(CAPSULES_LIST, CAPSULES_API_ADDRESS, self.capsules,
-                                                                                    LAUNCHES_LIST, LAUNCHES_API_ADDRESS, self.launches,
-                                                                                    first_object_key,
-                                                                                    second_object_key,
-                                                                                    replace_key)
-    def get_launches_data_for_boosters_state(self, BOOSTERS_LIST, BOOSTERS_API_ADDRESS,
-                                             LAUNCHES_LIST, LAUNCHES_API_ADDRESS,
-                                             first_object_key = "launches", second_object_key = "id", replace_key = "flight_number"):
-
-        self.boosters = self.replace_id_whith_elements_in_object_list_standard_state(BOOSTERS_LIST, BOOSTERS_API_ADDRESS, self.boosters,
-                                                                                     LAUNCHES_LIST, LAUNCHES_API_ADDRESS, self.launches,
-                                                                                     first_object_key,
-                                                                                     second_object_key,
-                                                                                     replace_key)
-    # ------------------------------------------------------------------------------------------------------------------
-    def execute_standard_key_value_state(self, OBJECT_LIST, OBJECT_KEYS_LIST, API_ADDRESS, objects, value = "", key = ""):
-        vehicles_objects = SpacexObjects(API_ADDRESS, OBJECT_LIST, objects)
-        objects = vehicles_objects.objects
-        sorted_vehicles = ObjectsSort(objects)
-        sorted_vehicles.print_objects_by_value_of_key(value, key, OBJECT_KEYS_LIST)
-        return objects
-
-    def replace_id_whith_elements_in_object_list_standard_state(self, OBJECT_LIST, API_ADDRESS, objects,
-                                                                OBJECT2_LIST, API_ADDRESS2, objects2,
-                                                                first_object_key, second_object_key, replace_key):
-        so = SpacexObjects(API_ADDRESS, OBJECT_LIST, objects)
-        objects = so.objects
-        so2 = SpacexObjects(API_ADDRESS2, OBJECT2_LIST, objects2)
-        objects2 = so2.objects
-
-        list_bufor = []
-        for object in objects:
-            if object.OBJECT_DICT[first_object_key] != []:
-                list_of_persons = list(object.OBJECT_DICT[first_object_key])
-                for id in list_of_persons:
-                    for person in objects2:
-                        if person.OBJECT_DICT[second_object_key] == id or person.OBJECT_DICT[replace_key] == id:
-                            list_bufor.append(person.OBJECT_DICT[replace_key])
-                    object.OBJECT_DICT[first_object_key] = list_bufor
-                list_bufor = []
-        print(">>> {} {} replaced by {} in object list.".format(first_object_key, second_object_key, replace_key))
-        return objects
+    slowdown = green.to(yellow)
+    stop = yellow.to(red)
+    go = red.to(green)
