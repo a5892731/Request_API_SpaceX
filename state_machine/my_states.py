f2b64c80384551308af87f5b54e6466a3c0f2ce6
a5892731 <a5892731@o2.pl>
2021-03-21 19:14:04 +0100

    in progress

--- state_machine/my_states.py
+++ state_machine/my_states.py
@@ -12,28 +12,28 @@ from state_machine.states.user_choice import UserChoiceBody
 
 # Start of our states <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 class Initialization(InitializationBody):
-    def on_event(self, event):
+    def on_event(self, event = 'device_locked'):
 
         if event == 'device_locked':
             return GetConnectionData()
         return self
 
 class GetConnectionData(GetConnectionDataBody):
-    def on_event(self, event):
+    def on_event(self, event = 'device_locked'):
 
         if event == 'device_locked':
             return GetDataFromApi()
         return self
 
 class GetDataFromApi(GetDataFrimApiBody):
-    def on_event(self, event):
+    def on_event(self, event = 'device_locked'):
 
         if event == 'device_locked':
             return UserChoice()
         return self
 
 class UserChoice(UserChoiceBody):
-    def on_event(self, event):
+    def on_event(self, event = 'device_locked'):
 
         if event == 'device_locked':
             return Initialization()
